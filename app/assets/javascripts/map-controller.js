a0a1411b668dfaf82e72a6ea811b3c53c9e4148c
katherineimogene <katherineimogene@gmail.com>
2014-04-26 21:30:46 -0700

    Basic tests for BootMap.Controller   * tests existence of a function to create a new map object.   * tests existence of a function to initialize an open street map (osm).

--- app/assets/javascripts/map-controller.js
+++ app/assets/javascripts/map-controller.js
@@ -1,3 +1,16 @@
 BootMap.Controller = function(){
 
 }
+
+BootMap.Controller.prototype = {
+  newMap: function(){
+    var map = {}
+    return map
+  },
+
+  osmInitializer: function(){
+    var osm = {}
+    return osm
+  }
+}
+
--- spec/javascripts/map_spec.js
+++ spec/javascripts/map_spec.js
@@ -15,3 +15,24 @@ describe("NameSpace", function(){
     expect(BootMap.View).toBeDefined()
   })
 })
+
+describe("Controller", function(){
+  beforeEach(function(){
+    controller = new BootMap.Controller
+    mapStub = {}
+    osmStub = {}
+  })
+
+  it("has a new map function", function(){
+    expect(controller.newMap()).toBeDefined()
+  }),
+
+  it("creates a new map object", function(){
+    expect(returnTypeOf(controller.newMap())).toBe(returnTypeOf(mapStub))
+  }),
+
+  it("initializes an OpenStreetMap layer", function(){
+    expect(returnTypeOf(controller.osmInitializer())).toBe(returnTypeOf(osmStub))
+  })
+
+})
