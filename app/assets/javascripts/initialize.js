e1e2245e98e47f674212a327e60d70f479979e42
katherineimogene <katherineimogene@gmail.com>
2014-04-27 09:08:30 -0700

    sets coordinates of initial map position

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -4,6 +4,6 @@ document.addEventListener('DOMContentLoaded', function(){
   map_view = new BootMap.View(map_controller)
   map_controller.view = map_view
   map_controller.fetchUsers()
-  map_controller.initializeMap(37.769, -5.429, 2)
+  map_controller.initializeMap(37.769, -5, 2)
   map_view.drawMap()
 });
